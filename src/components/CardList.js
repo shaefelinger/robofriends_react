555fd747398ff4547d9847561be11c0af68a8f47
Steffen Häfelinger <s.haefelinger@gmx.de>
2022-01-11 18:32:12 +0100

    errorBoundry

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,20 +2,23 @@ import React from 'react';
 import Card from '../components/Card';
 
 const CardList = ({ robots }) => {
-  return (
-    <div>
-      {robots.map((user, i) => {
-        return (
-          <Card
-            key={robots[i].id}
-            id={robots[i].id}
-            name={robots[i].name}
-            email={robots[i].email}
-          />
-        );
-      })}
-    </div>
-  );
+  if (true) {
+    throw new Error('Noooo!!!');
+  }
+  // return (
+  //   <div>
+  //     {robots.map((user, i) => {
+  //       return (
+  //         <Card
+  //           key={robots[i].id}
+  //           id={robots[i].id}
+  //           name={robots[i].name}
+  //           email={robots[i].email}
+  //         />
+  //       );
+  //     })}
+  //   </div>
+  // );
 };
 
 export default CardList;
